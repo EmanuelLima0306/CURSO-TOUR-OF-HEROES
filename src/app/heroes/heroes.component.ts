117411de62fd30297818aba7afd2143abbda7d85
EmanuelLima0306 <emanuellima.lnb@gmail.com>
2022-12-26 10:29:06 +0100

    edita um heroi

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../heroes.model';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+  hero: Hero = {id: 1, name: 'Wolverine'};
+  constructor(){}
+  ngOnInit(): void{
+
+  }
+}
